68dac1375410e2d76c4074ef32ec8f372db78b95
kingsly771 <lightkenshiro@gmail.com>
2025-08-29 13:15:59 -0700

    Update sessionRoutes.js

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const sessionController = require('../controllers/sessionController');
 
+const router = express.Router();
+
+router.post('/', sessionController.createSession);
+router.get('/:sessionId/status', sessionController.getSessionStatus);
+router.delete('/:sessionId', sessionController.disconnectSession);
+
+// Add endpoint to get bot prefix info
+router.get('/info/prefix', (req, res) => {
+  const config = req.app.get('config');
+  res.json({
+    prefix: config.bot.prefix,
+    adminPrefix: config.bot.adminPrefix
+  });
+});
+
+module.exports = router;
