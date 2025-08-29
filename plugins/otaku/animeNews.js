9421d30fc8198d7e0f16fd4602fb52538733cb92
kingsly771 <lightkenshiro@gmail.com>
2025-08-29 11:05:10 -0700

    Create animeNews.js

---
+++ plugins/otaku/animeNews.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: 'Anime News',
+  description: 'Shares the latest anime news and announcements',
+  version: '1.0.0',
+  code: `const animeNews = [
+  "New season of Attack on Titan announced!",
+  "Demon Slayer movie breaks box office records",
+  "One Piece chapter 1100 reveals shocking twist",
+  "Studio Ghibli announces new film for 2024",
+  "Crunchyroll adds 50 new titles to catalog"
+];
+
+async function animeNews(message, client, sessionId) {
+  if (message.body === '!animenews') {
+    const randomNews = animeNews[Math.floor(Math.random() * animeNews.length)];
+    await client.sendMessage(message.from, 
+      \`📰 Anime News: \${randomNews}\\n\\n` +
+      \`Stay tuned for more updates!\`
+    );
+  }
+  
+  if (message.body === '!latestnews') {
+    let response = '📰 Latest Anime News:\\n\\n';
+    for (let i = 0; i < Math.min(3, animeNews.length); i++) {
+      response += \`• \${animeNews[i]}\\n\`;
+    }
+    await client.sendMessage(message.from, response);
+  }
+}`
+};
