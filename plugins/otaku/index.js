8a90d391b5f989851271c246b401e0932d05e057
kingsly771 <lightkenshiro@gmail.com>
2025-08-29 10:48:56 -0700

    Create index.js

---
+++ plugins/otaku/index.js
@@ -0,0 +1,43 @@
+const animeRecommendation = require('./animeRecommendation');
+const mangaTracker = require('./mangaTracker');
+const characterInfo = require('./characterInfo');
+const cosplayEvents = require('./cosplayEvents');
+const animeQuotes = require('./animeQuotes');
+const watchParty = require('./watchParty');
+const mangaReleases = require('./mangaReleases');
+const animeNews = require('./animeNews');
+const waifuRating = require('./waifuRating');
+const husbandoRating = require('./husbandoRating');
+const animeTrivia = require('./animeTrivia');
+const animeOpenings = require('./animeOpenings');
+const mangaRecommendation = require('./mangaRecommendation');
+const animeSchedule = require('./animeSchedule');
+const cosplayContest = require('./cosplayContest');
+const animeMusic = require('./animeMusic');
+const japaneseLearning = require('./japaneseLearning');
+const animeMemes = require('./animeMemes');
+const virtualYouTubers = require('./virtualYouTubers');
+const animeArtShare = require('./animeArtShare');
+
+module.exports = {
+  animeRecommendation,
+  mangaTracker,
+  characterInfo,
+  cosplayEvents,
+  animeQuotes,
+  watchParty,
+  mangaReleases,
+  animeNews,
+  waifuRating,
+  husbandoRating,
+  animeTrivia,
+  animeOpenings,
+  mangaRecommendation,
+  animeSchedule,
+  cosplayContest,
+  animeMusic,
+  japaneseLearning,
+  animeMemes,
+  virtualYouTubers,
+  animeArtShare
+};
